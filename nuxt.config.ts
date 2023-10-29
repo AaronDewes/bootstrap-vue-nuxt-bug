74bec7b82a8aa2db1554640aa14065ac0405dca7
Aaron Dewes <aaron.dewes@protonmail.com>
2023-10-29 17:14:56 +0100

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    // Comment out the following line and the image will load properly
+    '@bootstrap-vue-next/nuxt',
+  ]
+})
